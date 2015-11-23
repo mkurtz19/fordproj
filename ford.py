e590b07e9195f0c04a5a00fb358133059a601c61
Marc Kurtz <mkurtz19@gmail.com>
2015-11-23 14:20:25 -0500

    knn added

--- ford.py
+++ ford.py
@@ -3,8 +3,9 @@ import numpy
 from sklearn import svm
 import matplotlib.pyplot as plt
 import pickle
+from sklearn import neighbors
 
-def train(infile):
+def trainSVC(infile):
     print "reading data from file"
 
     #cols = numpy.r_[3:33]
@@ -29,12 +30,12 @@ def train(infile):
 
     print "creating model"
 
-    model = svm.SVR(kernel='rbf', C=10, gamma=0.001, verbose=3)
+    model = svm.SVC(kernel='rbf', C=10, gamma=0.001, verbose=3)
     model.cache_size = 1024
 
     print "fitting data"
 
-    model.fit(x[0:100000,:],y[0:100000])
+    model.fit(x,y)
 
     print "saving model to model.pkl"
 
@@ -42,15 +43,54 @@ def train(infile):
 
     return {'model':model, 'x':x, 'y':y}
 
-def test():
-    model = pickle.load(open('model.pkl', 'rb'))
+def trainKNN(infile):
+    print "reading data from file"
 
-    tData = numpy.genfromtxt('fordTrain.csv', skip_header=1, delimiter=',')
+    #cols = numpy.r_[3:33]
+    tData = numpy.genfromtxt(infile, skip_header=1, delimiter=',')#, usecols=cols)
 
     print "finished reading file"
 
     x = tData[:,3:11]
     y = tData[:,2]
+
+    print "N: " + str(x.shape[0])
+
+    mins = x.min(0)
+    x = x - mins
+    x = x - (x.max(0) - x.min(0)) / 2
+    maxs = x.max(0)
+    maxs[maxs==0] = 1
+    x = 2 * x / maxs
+    #maxs = x.max(0)
+    #ranges = maxs - mins
+    #x = 2 * (x - mins) / ranges - 1;
+
+    print "creating model"
+
+    model = neighbors.KNeighborsClassifier(n_neighbors=8, weights='distance', algorithm='auto', n_jobs=-1, verbose=3)
+    model.cache_size = 1024
+
+    print "fitting data"
+
+    model.fit(x,y)
+
+    print "saving model to model.pkl"
+
+    pickle.dump(model, open('model.pkl', 'wb'))
+
+    return {'model':model, 'x':x, 'y':y}
+
+def test(modelfile, testfile, solutionsfile):
+    model = pickle.load(open(modelfile, 'rb'))
+
+    tData = numpy.genfromtxt(testfile, skip_header=1, delimiter=',')
+    sData = numpy.genfromtxt(solutionsfile, skip_header=1, delimiter=',')
+
+    print "finished reading file"
+
+    x = tData[:,3:11]
+    y = sData[:,2]
 
     mins = x.min(0)
     x = x - mins
@@ -59,9 +99,9 @@ def test():
     maxs[maxs==0] = 1
     x = 2 * x / maxs
 
-    py = numpy.array(y[100000:200000])
+    py = numpy.array(y)
 
-    preds = numpy.array(model.predict(x[100000:200000,:]))
+    preds = numpy.array(model.predict(x))
     preds[preds>0.5] = 1
     preds[preds<=0.5] = 0
 
@@ -88,5 +128,5 @@ def test():
     print "f1: " + str(f1)
 
 if __name__ == "__main__":
-    #ford = train('fordTrain.csv')
-    test();
+    ford = trainSVC('fordTrain.csv')
+    #test('model_p_cols_only_rvm.pkl', 'fordTest.csv', 'Solution.csv');
